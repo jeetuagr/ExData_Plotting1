4aafc4940c96226d3101ccdda98e18e673b7c0f3
Jeetesh Agrawal <jeetuagr@users.noreply.github.com>
2017-06-07 23:00:19 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,18 @@
+setwd("~/Desktop/R/ExData_Plotting1")
+downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+downloadFile <- "./Data/household_power_consumption.zip"
+householdFile <- "./Data/household_power_consumption.txt"
+## 
+if (!file.exists(householdFile)) {
+  download.file(downloadURL, downloadFile, method = "curl")
+  unzip(downloadFile, overwrite = T, exdir = "./Data")
+}
+
+data_full <- read.csv(householdFile, header=T, sep=';', na.strings="?",nrows=2075259,
+                       check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+hist(data1$Global_active_power, main="Global Active Power",
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
